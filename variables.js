7df88f398a2202ace5d165a6f5460ba8ed79f7aa
Daniel Lim <daniellimatyahoo@gmail.com>
2026-01-12 17:54:14 +0800

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,10 @@
+//These are Variables
+
+
+let fullName = "Arkh alios"
+let age = 19;
+let student = true;
+
+document.getElementById("p1").textContent = fullName;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${student}`;
